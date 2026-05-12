4ca42f2dc04ee9e832f577310e34106ccee603c4
Aleksandr Novikov <nvkv_ai@outlook.com>
2026-05-13 00:12:49 +0300

    init

---
+++ robot/main.go
@@ -0,0 +1,80 @@
+package main
+
+import "fmt"
+
+type Robot interface {
+	Move(int)
+	Turn(int)
+	Set(state)
+	Start()
+	Stop()
+}
+
+type position struct {
+	x int
+	y int
+}
+
+type state string
+
+const (
+	water state = "water"
+	soap  state = "soap"
+	brush state = "brush"
+)
+
+type Robo struct {
+	position position
+	angle    int
+	state    state
+}
+
+func New() *Robo {
+	return &Robo{}
+}
+
+func (r *Robo) Move(steps int) {
+	angle := r.angle
+
+	switch {
+	case angle == 0:
+		r.position.x += steps
+	case angle%90 == 0:
+		if angle > 0 {
+			r.position.y += steps
+		} else {
+			r.position.y -= steps
+		}
+	case angle%180 == 0:
+		r.position.x -= steps
+	}
+
+	fmt.Printf("POS %v %v\n", r.position.x, r.position.y)
+}
+
+func (r *Robo) Turn(angle int) {
+	r.angle += angle
+	fmt.Printf("ANGLE %v\n", r.angle)
+}
+
+func (r *Robo) Set(s state) {
+	r.state = s
+	fmt.Printf("STATE %v\n", s)
+}
+
+func (r *Robo) Start() {
+	fmt.Printf("START WITH %v\n", r.state)
+}
+
+func (r *Robo) Stop() {
+	fmt.Printf("STOP\n")
+}
+
+func main() {
+	r := New()
+	r.Turn(-90)
+	r.Set(soap)
+	r.Start()
+	r.Move(50)
+	r.Stop()
+}
